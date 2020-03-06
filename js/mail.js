5a606fc6b36935e17e0f58e80077c38d5a46d4de
Vadym23 <vadya-23@ukr.net>
2020-03-07 01:49:31 +0200

    day3 done

--- js/mail.js
+++ js/mail.js
@@ -13,6 +13,7 @@ const game = {
         count: [1, 2, 3, 4],
         size: [4, 3, 2, 1]
     },
+    collision: new Set(),
     generateShip() {
         for (let i = 0; i < this.optionShip.count.length; i++) {
             for (let j = 0; j < this.optionShip.count[i]; j++) {
@@ -35,17 +36,57 @@ const game = {
 
         if (direction) {
             x = Math.floor(Math.random() * 10);
-            y = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * (10 - shipSize));
         } else {
             x = Math.floor(Math.random() * 10);
-            y = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * (10 - shipSize));
         }
 
-        console.log('direction', direction);
+        for (let i = 0; i < shipSize; i++) {
+            if (direction) {
+                ship.location.push(x + '' +(y + i))
+            } else {
+                ship.location.push((x + i) + '' + y)
+            }
+            ship.hit.push('');
+        } 
+
+        if (this.checkCollision(ship.location)) {
+            return this.generateOptionsShip(shipSize)
+        }
+
+        this.addCollision(ship.location);
 
         return ship;
+    },
+    checkCollision(location) {
+        for (const coord of location) {
+            if (this.collision.has(coord)) {
+                return true;
+            }
+        }
+    },
+    addCollision(location) {
+        for (let i = 0; i < location.length; i++) {
+            const startCoordX = location[i][0] - 1;
+
+            for (let j = startCoordX; j < startCoordX + 3; j++) {
+                const  startCoordY = location[i][1] - 1;
+
+                for (let z = startCoordY; z < startCoordY + 3; z++) {
+
+                    if (j >= 0 && j < 10 && z >= 0 && z < 10)  {
+                        const coord = j + '' + z;
+                                this.collision.add(coord);   
+                    }
+                   
+                }
+            }
+        }
     }
 };
+
+
 
 const play = {
     record: localStorage.getItem('seaBattleRecord') || 0,
